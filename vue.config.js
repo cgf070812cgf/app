9aa68a33fb405388580b8c2b144a014bbffefdc5
cgf070812cgf <122876782@qq.com>
2022-09-16 15:20:07 +0800

    Update configuration

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  // 自动打开
+  devServer: {
+    host: 'localhost',
+    port: 8080,
+    https: false,
+    hot: false,
+    proxy: null
+  },
+
+  // 关闭eslint语法检测
+  lintOnSave: false,
+
 })
